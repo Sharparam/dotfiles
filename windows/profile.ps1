d6d473ff80d2416b8ccbf1c8cf88b2c29fd4c10a
Adam Hellberg <adam.hellberg@ninetech.com>
2017-11-03 16:48:17 +0100

    Fix profile load issue

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,5 +1,5 @@
 [console]::BackgroundColor = "DarkMagenta"
 
-Invoke-Expression -Command .\aliases.ps1
+Get-Content -Path "$env:USERPROFILE\dotfiles\windows\aliases.ps1" -Raw | Invoke-Expression
 
 Clear-Host
