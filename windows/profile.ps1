805433fdbc2013d71fe22a23eb13f0a3b343455d
Adam Hellberg <adam.hellberg@ninetech.com>
2019-06-27 18:34:58 +0200

    Remove theming from PS profile

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -27,39 +27,39 @@ $host.PrivateData.ProgressForegroundColor = 'Gray'
 $host.PrivateData.ProgressBackgroundColor = $background
 
 # Syntax coloring and highlighting
-Set-PSReadlineOption -TokenKind Comment -ForegroundColor         Gray
-Set-PSReadlineOption -TokenKind Comment -BackgroundColor         $background
+#Set-PSReadlineOption -TokenKind Comment -ForegroundColor         Gray
+#Set-PSReadlineOption -TokenKind Comment -BackgroundColor         $background
 
-Set-PSReadlineOption -TokenKind Keyword -ForegroundColor         DarkGreen
-Set-PSReadlineOption -TokenKind Keyword -BackgroundColor         $background
+#Set-PSReadlineOption -TokenKind Keyword -ForegroundColor         DarkGreen
+#Set-PSReadlineOption -TokenKind Keyword -BackgroundColor         $background
 
-Set-PSReadlineOption -TokenKind String -ForegroundColor          Gray
-Set-PSReadlineOption -TokenKind String -BackgroundColor          $background
+#Set-PSReadlineOption -TokenKind String -ForegroundColor          Gray
+#Set-PSReadlineOption -TokenKind String -BackgroundColor          $background
 
-Set-PSReadlineOption -TokenKind Operator -ForegroundColor        White
-Set-PSReadlineOption -TokenKind Operator -BackgroundColor        $background
+#Set-PSReadlineOption -TokenKind Operator -ForegroundColor        White
+#Set-PSReadlineOption -TokenKind Operator -BackgroundColor        $background
 
-Set-PSReadlineOption -TokenKind Variable -ForegroundColor        Green
-Set-PSReadlineOption -TokenKind Variable -BackgroundColor        $background
+#Set-PSReadlineOption -TokenKind Variable -ForegroundColor        Green
+#Set-PSReadlineOption -TokenKind Variable -BackgroundColor        $background
 
-Set-PSReadlineOption -TokenKind Command -ForegroundColor         White
-Set-PSReadlineOption -TokenKind Command -BackgroundColor         $background
+#Set-PSReadlineOption -TokenKind Command -ForegroundColor         White
+#Set-PSReadlineOption -TokenKind Command -BackgroundColor         $background
 
-Set-PSReadlineOption -TokenKind Parameter -ForegroundColor       White
-Set-PSReadlineOption -TokenKind Parameter -BackgroundColor       $background
+#Set-PSReadlineOption -TokenKind Parameter -ForegroundColor       White
+#Set-PSReadlineOption -TokenKind Parameter -BackgroundColor       $background
 
-Set-PSReadlineOption -TokenKind Type -ForegroundColor            Cyan
-Set-PSReadlineOption -TokenKind Type -BackgroundColor            $background
+#Set-PSReadlineOption -TokenKind Type -ForegroundColor            Cyan
+#Set-PSReadlineOption -TokenKind Type -BackgroundColor            $background
 
-Set-PSReadlineOption -TokenKind Number -ForegroundColor          Magenta
-Set-PSReadlineOption -TokenKind Number -BackgroundColor          $background
+#Set-PSReadlineOption -TokenKind Number -ForegroundColor          Magenta
+#Set-PSReadlineOption -TokenKind Number -BackgroundColor          $background
 
-Set-PSReadlineOption -TokenKind Member -ForegroundColor          Cyan
-Set-PSReadlineOption -TokenKind Member -BackgroundColor          $background
+#Set-PSReadlineOption -TokenKind Member -ForegroundColor          Cyan
+#Set-PSReadlineOption -TokenKind Member -BackgroundColor          $background
 
 Set-PSReadlineOption -BellStyle None
 
-Clear-Host
+#Clear-Host
 
 Get-Content -Path "$env:USERPROFILE\dotfiles\windows\aliases.ps1" -Raw | Invoke-Expression
 
