1cb7d1775240d0b9b02efe6d962939f1212fd522
Adam Hellberg <sharparam@sharparam.com>
2022-10-16 18:34:20 +0200

    Format doom config

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -47,8 +47,7 @@
 (setq projectile-project-search-path '(
                                         ("~/projects" . 1)
                                         ("~/repos" . 3)
-                                        ("~/.ghq" . 3)
-                                        ))
+                                        ("~/.ghq" . 3)))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
