d5a7fdb52a12e0097c63b8d6653742b20e42e1ec
Adam Hellberg <adam.hellberg@sharparam.com>
2023-06-05 17:22:22 +0200

    Update Packer boostrap for nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,12 +1,17 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({
-    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
-  })
-  vim.cmd 'packadd packer.nvim'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
+    })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
+local packer_bootstrap = ensure_packer()
 local packer = require 'packer'
 
 return packer.startup(function(use)
@@ -19,5 +24,11 @@ return packer.startup(function(use)
   use 'tpope/vim-repeat'
   use 'tpope/vim-surround'
 
+  use 'ggandor/leap.nvim'
+
   use 'tomasiser/vim-code-dark'
+
+  if packer_bootstrap then
+    packer.sync()
+  end
 end)
