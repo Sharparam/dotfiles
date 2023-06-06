d17a67d16d4c9145a7f3353301e7bec6740c7212
Adam Hellberg <sharparam@sharparam.com>
2023-06-06 23:01:47 +0200

    [nvim] Switch to tokyonight color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,41 @@
-return {
-  {
-    'Mofiqul/vscode.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      italic_comments = true,
-    },
-    config = function(_, opts)
-      require 'config.colorscheme'
-      local vscode = require 'vscode'
-      vscode.setup(opts)
-      vscode.load()
-    end
+local available = {
+  vscode = {
+    {
+      'Mofiqul/vscode.nvim',
+      lazy = false,
+      priority = 1000,
+      opts = {
+        italic_comments = true,
+      },
+      config = function(_, opts)
+        require 'config.colorscheme'
+        local vscode = require 'vscode'
+        vscode.setup(opts)
+        vscode.load()
+      end
+    }
+  },
+  tokyonight = {
+    {
+      'folke/tokyonight.nvim',
+      lazy = false,
+      priority = 1000,
+      opts = {
+        style = 'night', -- storm, night, moon, day
+        styles = {
+          comments = { italic = true },
+          sidebars = 'dark',
+          floats = 'dark'
+        },
+        sidebars = { 'NvimTree', 'qf', 'help', 'lazy' }
+      },
+      config = function(_, opts)
+        require 'config.colorscheme'
+        require('tokyonight').setup(opts)
+        vim.cmd [[colorscheme tokyonight]]
+      end
+    }
   }
 }
+
+return available.tokyonight
