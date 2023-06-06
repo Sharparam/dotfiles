2c9c133ec4da783d05b631c008b1740080b99159
Adam Hellberg <sharparam@sharparam.com>
2023-06-06 15:01:59 +0200

    [nvim] Disable netrw

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
+-- Disable netrw because we use nvim-tree instead
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
