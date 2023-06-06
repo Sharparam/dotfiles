645c0e0799ce5fd82a54f1b74d9a6329d9a45db6
Adam Hellberg <sharparam@sharparam.com>
2023-06-06 23:04:57 +0200

    [nvim] Update which-key bindings

--- nvim/.config/nvim/lua/config/which_key.lua
+++ nvim/.config/nvim/lua/config/which_key.lua
@@ -2,6 +2,65 @@ local wk = require 'which-key'
 
 wk.register({
   b = { name = '+buffer' },
+  c = {
+    name = '+code',
+    d = { vim.lsp.buf.definition, 'Jump to definition' },
+    D = { vim.lsp.buf.references, 'Jump to references' },
+    f = { function() vim.lsp.buf.format { async = true } end, 'Format buffer' },
+    i = { vim.lsp.buf.implementation, 'Find implementations' },
+    k = { vim.lsp.buf.hover, 'Show help' },
+    l = {
+      name = '+lsp',
+      ['='] = {
+        name = '+formatting',
+        ['='] = { function() vim.lsp.buf.format { async = true } end, 'Format buffer' }
+      },
+      a = {
+        name = '+code actions',
+        a = { vim.lsp.buf.code_action, 'Code actions' }
+      },
+      d = {
+        name = '+diagnostics',
+        l = { vim.diagnostic.open_float, 'Show diagnostics window' },
+        n = { vim.diagnostic.goto_next, 'Go to next diagnostic' },
+        p = { vim.diagnostic.goto_prev, 'Go to previous diagnostic' }
+      },
+      f = {
+        name = '+folders',
+        a = { vim.lsp.buf.add_workspace_folder, 'Add workspace folder' },
+        l = { vim.lsp.buf.list_workspace_folders, 'List workspace folders' },
+        r = { vim.lsp.buf.remove_workspace_folder, 'Remove workspace folder' }
+      },
+      g = {
+        name = '+goto',
+        a = { vim.lsp.buf.workspace_symbol, 'Find symbol in workspace' },
+        c = { vim.lsp.buf.incoming_calls, 'Find incoming calls' },
+        C = { vim.lsp.buf.outgoing_calls, 'Find outgoing calls' },
+        d = { vim.lsp.buf.declaration, 'Find declarations' },
+        g = { vim.lsp.buf.definition, 'Find definitions' },
+        i = { vim.lsp.buf.implementation, 'Find implementations' },
+        r = { vim.lsp.buf.references, 'Find references' },
+        t = { vim.lsp.buf.type_definition, 'Find type definition' }
+      },
+      G = { name = '+peek' },
+      h = {
+        name = '+help',
+        s = { vim.lsp.buf.signature_help, 'Show signature help' }
+      },
+      r = {
+        name = '+refactor',
+        r = { vim.lsp.buf.rename, 'Rename' }
+      },
+      t = {
+        name = '+toggle',
+        s = { vim.lsp.buf.signature_help, 'Show signature help' }
+      },
+      -- w = { name = '+workspaces' }
+    },
+    r = { vim.lsp.buf.rename, 'Rename' },
+    t = { vim.lsp.buf.type_definition, 'Find type definition' },
+    x = { name = '+Trouble' }
+  },
   f = { name = '+file' },
   g = {
     name = '+git',
@@ -14,6 +73,7 @@ wk.register({
       b = { '<Cmd>WhichKey<CR>', 'Show all mappings' }
     }
   },
+  i = { name = '+insert' },
   o = { name = '+open' },
   -- p = { name = '+project' },
   s = { name = '+search' }
