9eb7f5bbf57ca5b8468835854ff2121c0860c545
Adam Hellberg <sharparam@sharparam.com>
2023-06-06 15:01:33 +0200

    [nvim] Add some utils

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -66,5 +66,11 @@ function M.has(plugin)
   return require('lazy.core.config').plugins[plugin] ~= nil
 end
 
+function M.fg(name)
+  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+  local fg = hl and hl.fg or hl.foreground
+  return fg and { fg = string.format('#%06x', fg) }
+end
+
 _G.utils = M
 return M
