f17662120a6bfb727fd53d36f7d257703f8cf9dc
Adam Hellberg <sharparam@sharparam.com>
2025-02-02 04:33:42 +0100

    [nvim] Update community plugin config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -5,10 +5,34 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
-  { import = "astrocommunity.workflow.bad-practices-nvim" },
-  { import = "astrocommunity.workflow.precognition-nvim" }
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+
+  { import = "astrocommunity.indent.mini-indentscope" },
+
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.mini-surround" },
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.jj" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.just" },
+  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.mdx" }, -- Also adds Markdown
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.ps1" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- { import = "astrocommunity.workflow.bad-practices-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.workflow.precognition-nvim" },
   -- import/override with your plugins folder
 }
