43ed71dd3eb7bed68fd3d5eeba1e573e4c8a1a4b
Adam Hellberg <sharparam@sharparam.com>
2025-02-16 23:45:41 +0100

    [doom] Enable direnv

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -90,7 +90,7 @@
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
+       direnv
        ;;docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
