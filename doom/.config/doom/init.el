c0efa834ffe83b77947ff3cb275914bc4fb64ee9
Adam Hellberg <sharparam@sharparam.com>
2026-06-04 20:21:41 +0200

    [doom] Update init file

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -178,7 +178,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
+       (web +tree-sitter)  ; the tubes
        (yaml +lsp)              ; JSON, but readable
        ;;zig               ; C, but simpler
 
